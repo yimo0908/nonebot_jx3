0a8ea4f4d2fa42a7581ad312a1fddb6afd4af41a
yimo0908 <979856731@qq.com>
2020-12-23 17:53:21 +0800

    add 美人图

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import httpx
 import json
 api = f'https://jx3api.com/next/daily.php?token=jx3zhenhaowan&server=%E7%BB%9D%E4%BB%A3%E5%A4%A9%E9%AA%84'
 res = httpx.get(api)
-dict = json.loads(res.content)
-sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dict))
+dictionary = json.loads(res.content)
+weekday = ("{星期}".format(**dictionary))
+if weekday == ("一" or "二" or "四"):
+    sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
+else:
+    sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n美人图：{美人画像}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
 print(sendmsg)
