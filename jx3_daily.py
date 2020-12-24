25e5ceb6808f2132cbcaf93e32fc214787e1a8fb
Fly-Potato <live1006186250@live.com>
2020-12-24 18:02:29 +0800

    Update jx3_daily.py
    fixed bug

--- jx3_daily.py
+++ jx3_daily.py
@@ -7,15 +7,15 @@ async def jx3_daily(session):
     user = session.event.user_id
     fwq = "绝代天骄"
     api = f'https://jx3api.com/next/daily.php?token=jx3zhenhaowan&server={fwq}'
-    with httpx.Client() as s:
+    async with httpx.Client() as s:
         try:
-            res = s.get(api)
+            res = await s.get(api)
         except Exception as e:
             msg = str(e)
             # print(msg)
             await session.send(f'[CQ:at,qq={user}]' + '消息处理失败\n' + msg)
             return
-    
+
     dictionary = res.json()
     msg = ''
     for key in dictionary:
