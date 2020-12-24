2095991e326d10109536b7a5b2f57745d34b9d10
Fly-Potato <live1006186250@live.com>
2020-12-24 17:50:43 +0800

    Update jx3_daily.py
    修改代码结构

--- jx3_daily.py
+++ jx3_daily.py
@@ -1,7 +1,5 @@
 from nonebot import on_command
-
 import httpx
-import json
 
 
 @on_command('日常', only_to_me=False)
@@ -9,10 +7,18 @@ async def jx3_daily(session):
     user = session.event.user_id
     fwq = "绝代天骄"
     api = f'https://jx3api.com/next/daily.php?token=jx3zhenhaowan&server={fwq}'
-    res = httpx.get(api)
-    dictionary = json.loads(res.content)
-    if "美人画像" not in dictionary.keys():
-        sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
-    else:
-        sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n美人图：{美人画像}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
-    await session.send(f'[CQ:at,qq={user}]' + "今日日常：\n" + sendmsg)
+    with httpx.Client() as s:
+        try:
+            res = s.get(api)
+        except Exception as e:
+            msg = str(e)
+            # print(msg)
+            await session.send(f'[CQ:at,qq={user}]' + '消息处理失败\n' + msg)
+            return
+    
+    dictionary = res.json()
+    msg = ''
+    for key in dictionary:
+        msg += "%s: %s\n" % (key, dictionary[key])
+    # print(msg)
+    await session.send(f'[CQ:at,qq={user}]' + "今日日常：\n" + msg)
