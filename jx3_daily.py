4d46b84413fd3a43910c5340666bf8f403c1d156
yimo0908 <979856731@qq.com>
2020-12-24 14:57:47 +0800

    修改美人图的判断方式

--- jx3_daily.py
+++ jx3_daily.py
@@ -7,12 +7,11 @@ import json
 @on_command('日常', only_to_me=False)
 async def jx3_daily(session):
     user = session.event.user_id
-    fwq="绝代天骄"
+    fwq = "绝代天骄"
     api = f'https://jx3api.com/next/daily.php?token=jx3zhenhaowan&server={fwq}'
     res = httpx.get(api)
     dictionary = json.loads(res.content)
-    weekday = ("{星期}".format(**dictionary))
-    if weekday == ("一" or "二" or "四"):
+    if "美人画像" not in dictionary.keys():
         sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
     else:
         sendmsg = ("时间：{时间} 周{星期}\n大战：{秘境大战}\n战场：{今日战场}\n驰援：{驰援任务}\n美人图：{美人画像}\n周常公共任务：{武林通鉴·公共任务}\n周常5人本：{武林通鉴·秘境任务}\n周常10人本：{武林通鉴·团队秘境}".format(**dictionary))
