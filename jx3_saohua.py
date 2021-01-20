b5e794f705fb2c72e0e13f1311e1e30a4d8d5943
yimo <979856731@qq.com>
2021-01-20 13:32:30 +0800

    Update jx3_saohua.py

--- jx3_saohua.py
+++ jx3_saohua.py
@@ -6,7 +6,6 @@ import json
 @on_command('骚话', only_to_me=False)
 async def jx3_sanhua(session):
     api = f'https://jx3api.com/api/random.php?token=153166341'
-    async with httpx.AsyncClient() as sess:
-        res = sess.get(api)
+    res = httpx.get(api)
     dictionary = res.json()
     await session.send(dictionary["data"]["text"])
