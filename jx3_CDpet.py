f30baa47db876a72bc023a602a07dfecdcedff50
yimo <979856731@qq.com>
2021-01-21 12:40:29 +0800

    Update jx3_CDpet.py

--- jx3_CDpet.py
+++ jx3_CDpet.py
@@ -8,44 +8,30 @@ from time import strftime, localtime
 from nonebot import on_command
 
 
-def get_msg(what):
-    api = 'https://next.jx3box.com/api/serendipity?server=绝代天骄&role=&serendipity=' + \
-        what+'&start=0&pageIndex=1&pageSize=1'
-    res = httpx.get(api)
-    data = json.loads(res.content)
-    dictionary = data["data"]["data"]
-    for i, subject in enumerate(dictionary):
-        one_subject = "%s  上个CD：%s" % (
-            subject["serendipity"], subject["date_str"])
-    return one_subject
-
-
-who1 = {
-    "哈皮", "嘟嘟", "小灰", "小锦", "白鹅", "稻稻", "蟹仔", "蟹仕", "蟹兵", "蟹卒", "蟹士", "蟹将", "蟹帅", "蟹炮", "蟹相", "蟹砲", "蟹象", "阿里", "阿飞",
-    "静静"
-}
-who2 = {
-    "大头鹅",
-    "蟹车·红",
-    "蟹车·蓝",
-    "蟹马·红",
-    "蟹马·蓝",
-}
-time = "\n统计时间：" + strftime("%Y-%m-%d %H:%M:%S", localtime())
-title = "绝代天骄的蹲宠CD如下：\n"
-
-sendmsg1 = ""
-for i in who1:
-    sendmsg1 += get_msg(i) + "\n"
-
-sendmsg2 = ""
-for i in who2:
-    sendmsg2 += get_msg(i) + "\n"
-
-root = os.getcwd()
-
 @on_command('蹲宠', only_to_me=False)
 async def jx3_CDpet(session):
+    def get_msg(what):
+        api = 'https://next.jx3box.com/api/serendipity?server=绝代天骄&role=&serendipity=' + \
+            what+'&start=0&pageIndex=1&pageSize=1'
+        res = httpx.get(api)
+        data = json.loads(res.content)
+        dictionary = data["data"]["data"]
+        for i, subject in enumerate(dictionary):
+            one_subject = "%s  上个CD：%s" % (
+                subject["serendipity"], subject["date_str"])
+        return one_subject
+    who1 = {"哈皮", "嘟嘟", "小灰", "小锦", "白鹅", "稻稻", "蟹仔", "蟹仕", "蟹兵", "蟹卒",
+            "蟹士", "蟹将", "蟹帅", "蟹炮", "蟹相", "蟹砲", "蟹象", "阿里", "阿飞", "静静"}
+    who2 = {"大头鹅", "蟹车·红", "蟹车·蓝", "蟹马·红", "蟹马·蓝", }
+    time = "\n统计时间：" + strftime("%Y-%m-%d %H:%M:%S", localtime())
+    title = "绝代天骄的蹲宠CD如下：\n"
+    sendmsg1 = ""
+    for i in who1:
+        sendmsg1 += get_msg(i) + "\n"
+    sendmsg2 = ""
+    for i in who2:
+        sendmsg2 += get_msg(i) + "\n"
+    root = os.getcwd()
     msg = title + sendmsg1 + sendmsg2 + time
     bk_img = cv2.imread("bk.png")
     # 设置需要显示的字体
