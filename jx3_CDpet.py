f18485eb6e96f6ddeb6c0a53094d739494b2c64d
Potato <live1006186250@live.com>
2021-01-22 02:12:26 +0800

    update pet (#8)
    减少不必要的库
    修改时间显示

--- jx3_CDpet.py
+++ jx3_CDpet.py
@@ -1,46 +1,59 @@
 import httpx
 import json
 import os
-import cv2
 from PIL import ImageFont, ImageDraw, Image
-import numpy as np
 from time import strftime, localtime
+import datetime
 from nonebot import on_command
 
 
+def get_pet_time(name):
+    api = 'https://next.jx3box.com/api/serendipity?server=绝代天骄&role=&serendipity=' + \
+          name + '&start=0&pageIndex=1&pageSize=1'
+    res = httpx.get(api)
+    data = json.loads(res.content)
+    dictionary = data["data"]["data"]
+    for i, subject in enumerate(dictionary):
+        _time = datetime.datetime.strptime(subject["date_str"], '%Y-%m-%d %H:%M:%S')
+        _time = datetime.datetime.now() - _time
+        mm, ss = divmod(_time.seconds, 60)
+        hh, mm = divmod(mm, 60)
+        s = "%d小时:%02d分:%02d秒" % (hh, mm, ss)
+        if _time.days:
+            def plural(n):
+                return n, abs(n) != 1 and "s" or ""
+            s = ("%d 天%s, " % plural(_time.days)) + s
+        one_subject = "%s  上个CD：%s前" % (subject["serendipity"], s)
+    return one_subject
+
+
 @on_command('蹲宠', only_to_me=False)
-async def jx3_CDpet(session):
-    def get_msg(what):
-        api = 'https://next.jx3box.com/api/serendipity?server=绝代天骄&role=&serendipity=' + \
-            what+'&start=0&pageIndex=1&pageSize=1'
-        res = httpx.get(api)
-        data = json.loads(res.content)
-        dictionary = data["data"]["data"]
-        for i, subject in enumerate(dictionary):
-            one_subject = "%s  上个CD：%s" % (
-                subject["serendipity"], subject["date_str"])
-        return one_subject
-    who1 = {"哈皮", "嘟嘟", "小灰", "小锦", "白鹅", "稻稻", "蟹仔", "蟹仕", "蟹兵", "蟹卒",
-            "蟹士", "蟹将", "蟹帅", "蟹炮", "蟹相", "蟹砲", "蟹象", "阿里", "阿飞", "静静"}
-    who2 = {"大头鹅", "蟹车·红", "蟹车·蓝", "蟹马·红", "蟹马·蓝", }
+def jx3_CDpet(session):
+    who1 = ["哈皮", "嘟嘟", "小灰", "小锦", "白鹅", "稻稻", "蟹仔", "蟹仕", "蟹兵", "蟹卒",
+            "蟹士", "蟹将", "蟹帅", "蟹炮", "蟹相", "蟹砲", "蟹象", "阿里", "阿飞", "静静"]
+    who2 = ["大头鹅", "蟹车·红", "蟹车·蓝", "蟹马·红", "蟹马·蓝"]
     time = "\n统计时间：" + strftime("%Y-%m-%d %H:%M:%S", localtime())
     title = "绝代天骄的蹲宠CD如下：\n"
     sendmsg1 = ""
-    for i in who1:
-        sendmsg1 += get_msg(i) + "\n"
+    for name in who1:
+        sendmsg1 += get_pet_time(name) + "\n"
     sendmsg2 = ""
-    for i in who2:
-        sendmsg2 += get_msg(i) + "\n"
-    root = os.getcwd()
+    for name in who2:
+        sendmsg2 += get_pet_time(name) + "\n"
+
     msg = title + sendmsg1 + sendmsg2 + time
-    bk_img = cv2.imread("bk.png")
+    img = Image.new('RGB', (700, 950), (255, 255, 255))
     # 设置需要显示的字体
-    fontpath = "font.ttc"
-    font = ImageFont.truetype(fontpath, 32)
-    img_pil = Image.fromarray(bk_img)
-    draw = ImageDraw.Draw(img_pil)
+    font_path = "font.ttc"
+    font = ImageFont.truetype(font_path, 32)
+    draw = ImageDraw.Draw(img)
     # 绘制文字信息
     draw.text((10, 10), msg, font=font, fill=(0, 0, 0))
-    bk_img = np.array(img_pil)
-    cv2.imwrite("dunchong.jpg", bk_img)
-    await session.send(f"[CQ:image,file=file:///{root}//dunchong.jpg]")
+    img.save("dunchong.jpg", 'jpeg')
+    root = os.getcwd()
+    await session.send(f"[CQ:image,file=file:///{root}\\dunchong.jpg]")
+
+
+
+if __name__ == '__main__':
+    jx3_CDpet()
