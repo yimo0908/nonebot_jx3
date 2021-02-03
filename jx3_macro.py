b0fa49e70134ee872bfe5dcfa7a6eeb28f48cd30
yimo <979856731@qq.com>
2021-02-03 19:57:05 +0800

    Create jx3_macro.py

---
+++ jx3_macro.py
@@ -0,0 +1,178 @@
+import httpx
+from nonebot import on_command
+
+
+@on_command('冰心宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=冰心诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('气纯宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=紫霞功'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('剑纯宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=太虚剑意'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('花间宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=花间游'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('易筋宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=易筋经'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('洗髓宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=洗髓经'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('傲血宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=傲血战意'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('铁牢宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=铁牢律'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('藏剑宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=问水诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('毒经宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=毒经'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('惊羽宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=惊羽诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('天罗宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=天罗诡道'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('丐帮宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=笑尘诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('分山宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=分山劲'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('铁骨宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=铁骨衣'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('焚影宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=焚影圣诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('明尊宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=明尊琉璃体'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('莫问宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=莫问'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('霸刀宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=北傲诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('蓬莱宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=凌海诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('凌雪宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=隐龙诀'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
+
+
+@on_command('衍天宏', only_to_me=False)
+async def jx3_macro(session):
+    api = f'https://jx3api.com/api/macro.php?token=153166341&name=太玄经'
+    res = httpx.get(api)
+    macro = res.text
+    await session.send(macro)
