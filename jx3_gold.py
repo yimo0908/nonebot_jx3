fd812d4c8be8ad0f73b886294737a41a1866c76c
yimo <979856731@qq.com>
2021-03-04 16:37:09 +0800

    Update jx3_gold.py

--- jx3_gold.py
+++ jx3_gold.py
@@ -43,5 +43,5 @@ async def get_gold_of_server(server: str) -> str:
         return '消息处理失败，请检查输入的服务器名称是否正确！'
     else:
         d = dictionary["data"]
-        msg ="%s的金价：\n万宝楼：%s\n游募：%s\nuu898：%s\ndd373：%s\n5173：%s\n7881：%s" % (d['server'], d['wanbaolou'], d['youmu'], d['uu898'], d['dd373'], d['5173'], d['7881'])
+        send_msg ="%s的金价：\n万宝楼：%s\n游募：%s\nuu898：%s\ndd373：%s\n5173：%s\n7881：%s" % (d['server'], d['wanbaolou'], d['youmu'], d['uu898'], d['dd373'], d['5173'], d['7881'])
     return send_msg
